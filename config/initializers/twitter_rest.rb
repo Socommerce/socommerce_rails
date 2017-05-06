6e42e40c60231ef86ef6674cf88f98f75c141fb7
vigneshwaran <vigneshsika@gmail.com>
2017-05-06 20:27:51 +0530

    Add initializers for twitter rest api

---
+++ config/initializers/twitter_rest.rb
@@ -0,0 +1,6 @@
+client = Twitter::REST::Client.new do |config|
+	config.consumer_key        = 'a0UmBU7AHn0fFneQ10zYwP0Kk'
+  config.consumer_secret     = 'JZEqUgKJsTrYjjvYv1wO4jtrNpy50pwoTUwr4UAro3pPxS7Hhw'
+  config.access_token        = '3139928784-rExJZIOTg77V1833ptq7YKbMq4rfok8y4AnIkfd'
+  config.access_token_secret = 'Z8USEigXd6l1KwCywJ9oH3inoQha3zRSNUCSdrxv25tBz'
+end	
