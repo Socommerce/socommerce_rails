68c2ae2d60ecbd5cab7172d7b2d1f1be3379666c
vigneshwaran <vigneshsika@gmail.com>
2017-05-06 22:53:30 +0530

    Add direct message to users for acknowledgements

--- config/initializers/twitter_rest.rb
+++ config/initializers/twitter_rest.rb
@@ -1,4 +1,4 @@
-client = Twitter::REST::Client.new do |config|
+@client = Twitter::REST::Client.new do |config|
 	config.consumer_key        = 'a0UmBU7AHn0fFneQ10zYwP0Kk'
   config.consumer_secret     = 'JZEqUgKJsTrYjjvYv1wO4jtrNpy50pwoTUwr4UAro3pPxS7Hhw'
   config.access_token        = '3139928784-rExJZIOTg77V1833ptq7YKbMq4rfok8y4AnIkfd'
